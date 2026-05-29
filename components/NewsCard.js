85a7d1593b8fa2114f6ed94516257f1299ad25d1
NelleBr <r1060145@student.thomasmore.be>
2026-05-29 14:44:42 +0200

    nelle-eindopdracht-newscard

---
+++ components/NewsCard.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+
+const NewsCard = ({
+  title,
+  description,
+  image,
+  category,
+  campus,
+  date,
+  onPress,
+}) => {
+  return (
+    <View style={styles.card}>
+      <Image source={image} style={styles.image} />
+
+      <View style={styles.topInfo}>
+        <Text style={styles.category}>{category}</Text>
+        <Text style={styles.topInfo}> • {campus}</Text>
+      </View>
+
+      <Text style={styles.date}>{date}</Text>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.description}>{description}</Text>
+
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>Lees meer</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 20,
+    marginBottom: 25,
+    overflow: "hidden",
+    borderLeftWidth: 5,
+    borderLeftColor: "#86BC25",
+  },
+
+  image: {
+    width: "100%",
+    height: 220,
+    resizeMode: "cover",
+  },
+
+  metaRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 16,
+    marginHorizontal: 16,
+  },
+
+  category: {
+    backgroundColor: "#EDF7E6",
+    color: "#86BC25",
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    borderRadius: 20,
+    fontWeight: "bold",
+  },
+
+  metaText: {
+    color: "#6B7280",
+    marginLeft: 10,
+    fontSize: 15,
+  },
+
+  date: {
+    color: "#6B7280",
+    marginTop: 10,
+    marginHorizontal: 16,
+    fontSize: 15,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#000000",
+    marginTop: 12,
+    marginHorizontal: 16,
+  },
+
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#4B5563",
+    marginTop: 8,
+    marginHorizontal: 16,
+  },
+
+  button: {
+    paddingVertical: 16,
+    marginHorizontal: 16,
+  },
+
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#86BC25",
+  },
+});
+
+export default NewsCard;
